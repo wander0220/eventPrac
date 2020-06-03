1f0706319d7e9ef52bcea244322d4c959674ce9c
wander0220 <s2018s26@e-mirim.hs.kr>
2020-06-03 13:58:29 +0900

    재밌게 공부하고 싶음 GUI 가지고 공부하기~~

--- eventPrac/Form1.cs
+++ eventPrac/Form1.cs
@@ -16,5 +16,11 @@ namespace eventPrac
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "♠";
+            label1.Text += "♠";
+        }
     }
 }
